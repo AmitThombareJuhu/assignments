75cee39544840b6ce208005b471e0e7579102604
AmitThombareJuhu <amit22thom@gmail.com>
2025-02-26 22:30:18 +0530

    Add files via upload

---
+++ java progamming assingment/Q-3/DivideNumbers.java
@@ -0,0 +1,8 @@
+public class DivideNumbers {
+	public static void main(String[] args){
+		int dividend = 50;
+		int divisor = 3;
+		int quotient = dividend/divisor;
+		System.out.println(quotient);	
+	}
+}
---
+++ java progamming assingment/Q-9/Areaofcircle.java
@@ -0,0 +1,8 @@
+class Areaofcircle {
+  public static void main(String[] args) {
+    int rad = 7;
+    float area = 3.14159f * rad * rad;
+    System.out.println("Area of circle is :" + (float) area);
+  }
+
+}
